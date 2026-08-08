b23a64fbbac9e5ab0f3eb388dbce0e6cb16e2311
nikvn89 <nikkyvn89@gmail.com>
2026-08-08 18:44:32 +0700

    Update genlayer.ts

--- frontend/src/lib/genlayer.ts
+++ frontend/src/lib/genlayer.ts
@@ -1,14 +1,14 @@
 import { createClient } from 'genlayer-js';
-import { studio } from 'genlayer-js/chains';
+import { localnet } from 'genlayer-js/chains';
 
 export const CONTRACT_ADDRESS = '0xbcf9EE06A7Cb5bb74Da57b71F7dBfe4081BA09e3';
 
 export const getClient = (account?: string) => {
   if (account) {
     return createClient({
-      chain: studio,
+      chain: localnet,
       account: account as `0x${string}`,
     });
   }
-  return createClient({ chain: studio });
+  return createClient({ chain: localnet });
 };

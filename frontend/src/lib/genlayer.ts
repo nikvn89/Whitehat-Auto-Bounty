b38b3572e850102256b79fb10b2f21aa55a03b48
nikvn89 <nikkyvn89@gmail.com>
2026-08-08 18:37:54 +0700

    Add files via upload

--- frontend/src/lib/genlayer.ts
+++ frontend/src/lib/genlayer.ts
@@ -1,17 +1,14 @@
 import { createClient } from 'genlayer-js';
-import { studionet } from 'genlayer-js/chains';
+import { simulator } from 'genlayer-js/chains';
 
-export const CONTRACT_ADDRESS = '0xBB9503a2Df9A90561f8CD2679B25369980D7FfF3';
+export const CONTRACT_ADDRESS = '0xbcf9EE06A7Cb5bb74Da57b71F7dBfe4081BA09e3';
 
 export const getClient = (account?: string) => {
-  // @ts-ignore
-  const provider = typeof window !== 'undefined' ? window.ethereum : undefined;
   if (account) {
     return createClient({
-      chain: studionet,
+      chain: simulator,
       account: account as `0x${string}`,
-      provider
     });
   }
-  return createClient({ chain: studionet, provider });
+  return createClient({ chain: simulator });
 };
